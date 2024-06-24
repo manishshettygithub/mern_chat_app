64c5cedb27e64b2ad267bce5ea060d20a53f14d9
Manish <manishhsetty1221@gmail.com>
2024-06-24 11:40:47 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use("/api/users",userRoutes);
 //deploy
 app.use(express.static(path.join(__dirname,"/frontend/dist")))
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/frontend/dist/index.html"))
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
     })
 
 server.listen(PORT,()=> {
