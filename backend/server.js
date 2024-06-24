3bbb76c7ccb9a8052cec51c41fc9d45d893e50e4
Manish <manishhsetty1221@gmail.com>
2024-06-24 10:05:16 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,16 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app,server } from "./socket/socket.js";
+import path from "path"
 
 
 
 const PORT=process.env.PORT || 5000;
+//deployeement 
+
+
+const __dirname=path.resolve()
+
 dotenv.config();
 
 
@@ -39,6 +45,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+//deploy
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/frontend/dist/index.html"))
+    })
 
 server.listen(PORT,()=> {
     connectToMongoDB();
